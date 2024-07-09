092cf9a45501dc5f83e513497fe7408cdb0daee2
eaysin-arafat <eaysin.arafat.j@gmail.com>
2024-07-09 23:19:10 +0600

    feat: initialize all files

--- src/object/index.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Utility functions for manipulating objects.
- * This module exports various functions for common object operations.
- */
--- src/object/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Utility functions for manipulating objects.
- * This module exports various functions for common object operations.
- */
-//# sourceMappingURL=index.js.map
